b1140c84898031f7d61adac474de3b30f286443a
Fraser <frasermince@gmail.com>
2015-06-23 16:29:27 -0500

    add test for code action creators

---
+++ test/actions/codeActionCreatorSpec.js
@@ -0,0 +1,20 @@
+let expect = require('chai').expect;
+let Application = require('../../app/application.js');
+let CodeConstants = require('../../app/constants/codeConstants');
+let { hasDispatched, createApplication } = require('marty/test-utils');
+
+describe('codeActionCreators', () => {
+  describe('#clickLine(lineNumber)', () => {
+    let app;
+    beforeEach(() => {
+      app = createApplication(Application, {
+        include: ['codeActionCreators']
+      });
+      app.codeActionCreators.clickLine(1);
+    });
+
+    it('should dispatch CLICK_LINE', function () {
+      expect(hasDispatched(app, CodeConstants.CLICK_LINE, 1)).to.be.true;
+    });
+  })
+});
